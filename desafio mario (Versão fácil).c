f85b407648231030d3d873a6d192ca2395212054
Leonardo Leitão <leoleitao27@gmail.com>
2022-12-18 10:55:43 -0300

    desafio mario

--- desafio mario (Versão fácil).c
+++ desafio mario (Versão fácil).c
@@ -3,18 +3,20 @@
 int main(){
 	int tamanhoTorre = 0;
 	int espacosVazios = 0;
+	int i,i2,i3;
 	
 	printf("Qual a altura da torre?\n");
 	scanf("%d",&tamanhoTorre);
+	tamanhoTorre++;
 	
 	espacosVazios = tamanhoTorre;
 	
-	for(int i = 1;i<tamanhoTorre;i++){
-		for(int i2 = 0;i2<espacosVazios;i2++ ){
+	for(i = 1;i<tamanhoTorre;i++){
+		for(i2 = 0;i2<espacosVazios;i2++ ){
 			printf(" ");
 		}
 		
-		for(int i3 = 1;i3<=i;i3++){
+		for(i3 = 1;i3<=i;i3++){
 			printf("#");
 		}
 		espacosVazios--;
